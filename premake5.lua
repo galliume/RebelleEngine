600427424591d51de1767d698e937c786e554f82
gcconantc <gcconantc@gmail.com>
2020-10-30 23:00:19 +0100

    add premake config

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Rebelle"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Rebelle"
+	location "Rebelle"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir  ("bin/" .. outputdir  .. "/%{prj.name}")
+	objdir  ("bin-int/" .. outputdir  .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.18362.0"
+
+		defines
+		{
+			"RBL_PLATFORM_WINDOWS",
+			"RBL_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "RBL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RBL_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "RBL_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir  ("bin/" .. outputdir  .. "/%{prj.name}")
+	objdir  ("bin-int/" .. outputdir  .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Rebelle/vendor/spdlog/include",
+		"Rebelle/src"
+	}
+
+	links 
+	{
+		"Rebelle"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.18362.0"
+
+		defines
+		{
+			"RBL_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "RBL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RBL_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "RBL_DIST"
+		optimize "On"
