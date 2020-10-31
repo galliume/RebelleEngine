5c6784d53776fa37d5a41e2c164c7034e0411017
gcconantc <gcconantc@gmail.com>
2020-10-31 11:58:24 +0100

    start Events

--- Rebelle/src/Rebelle/Application.cpp
+++ Rebelle/src/Rebelle/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Rebelle/Events/ApplicationEvent.h"
+#include "Rebelle/Log.h"
+
 namespace Rebelle {
 
 	Application::Application()
@@ -12,6 +15,18 @@ namespace Rebelle {
 
 	void Application::Run()
 	{
+		//test events
+		WindowResizeEvent e(1200, 720);
+
+		if (e.IsInCategory(EventCategoryApplication)) 
+		{
+			RBL_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			RBL_TRACE(e);
+		}
+
 		while (true) 
 		{
 
