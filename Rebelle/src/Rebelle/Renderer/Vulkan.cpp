dbbacc58ff1c06209a30006b19d283f9bc42c9e8
gcconantc <gcconantc@gmail.com>
2020-11-11 01:02:11 +0100

    add device and queue

--- Rebelle/src/Rebelle/Renderer/Vulkan.cpp
+++ Rebelle/src/Rebelle/Renderer/Vulkan.cpp
@@ -114,6 +114,62 @@ namespace Rebelle {
             RBL_CORE_FATAL("failed to create Vulkan instance!");
         }
     }
+    QueueFamilyIndices  Vulkan::findQueueFamilies(VkPhysicalDevice device)
+    {
+        QueueFamilyIndices indices;
+        
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        int i = 0;
+        for (const auto& queueFamily : queueFamilies) 
+        {
+            if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) 
+            {
+                indices.graphicsFamily = i;
+            }
+            if (indices.isComplete()) 
+            {
+                break;
+            }
+            i++;
+        }
+
+        return indices;
+    }
+    void Vulkan::pickPhysicalDevice()
+    {
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+        if (deviceCount == 0) 
+        {
+            throw std::runtime_error("No compatible GPU found");
+        }
+
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+        for (const auto& device : devices) {
+            if (isDeviceSuitable(device)) {
+                physicalDevice = device;
+                break;
+            }
+        }
+
+        if (physicalDevice == VK_NULL_HANDLE) {
+            throw std::runtime_error("No GPU can run this program");
+        }
+    }
+    bool Vulkan::isDeviceSuitable(VkPhysicalDevice device)
+    {
+        QueueFamilyIndices indices = findQueueFamilies(device);
+
+        return indices.isComplete();
+    }
     void Vulkan::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) 
     {
         createInfo = {};
--- Rebelle/src/Rebelle/Renderer/Vulkan.h
+++ Rebelle/src/Rebelle/Renderer/Vulkan.h
@@ -3,8 +3,15 @@
 #include "rblpch.h"
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <optional>
 
 namespace Rebelle {
+	struct QueueFamilyIndices 
+	{
+		std::optional<uint32_t> graphicsFamily;
+		bool isComplete() { return graphicsFamily.has_value(); }
+	};
+
 	class Vulkan
 	{
 	public:
@@ -15,11 +22,15 @@ namespace Rebelle {
 		VkDebugUtilsMessengerEXT callback;
 		bool enableValidationLayers = true;
 		const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
+		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 	private:
 		void createInstance();
+		QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+		void pickPhysicalDevice();
+		bool isDeviceSuitable(VkPhysicalDevice device);
 		void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
-		bool checkValidationLayerSupport();
 		void setupDebugMessenger();
+		bool checkValidationLayerSupport();
 		std::vector<const char*> getRequiredExtensions();
 	};
 }
